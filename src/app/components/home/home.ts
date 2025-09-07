166e17fbd1dd9c754b2a308d1347722dc215200e
Tom <tom.weelborg@gmail.com>
2025-09-07 17:40:58 +0200

    feat: add services component

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Info } from "./info/info";
 import { About } from "./about/about";
+import { Services } from "./services/services";
 
 @Component({
   selector: 'app-home',
-  imports: [Info, About],
+  imports: [Info, About, Services],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
