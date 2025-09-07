0d544aa3ca0ec86f0cd84398d2c1158da91fc6e5
Tom <tom.weelborg@gmail.com>
2025-09-07 17:47:56 +0200

    feat: add contact component

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Info } from "./info/info";
 import { About } from "./about/about";
 import { Services } from "./services/services";
+import { Contact } from "./contact/contact";
 
 @Component({
   selector: 'app-home',
-  imports: [Info, About, Services],
+  imports: [Info, About, Services, Contact],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
