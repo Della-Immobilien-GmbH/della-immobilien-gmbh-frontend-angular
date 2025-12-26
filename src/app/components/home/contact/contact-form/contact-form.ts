ac309abb96c0cc13aa52a33574b0e3e4940a85cf
Tom <tom.weelborg@gmail.com>
2025-12-26 15:14:18 +0100

    feat: add success symbol for submitting contact form

--- src/app/components/home/contact/contact-form/contact-form.ts
+++ src/app/components/home/contact/contact-form/contact-form.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnDestroy } from '@angular/core';
+import { Component, ElementRef, OnDestroy, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { environment } from '../../../../../environments/environment';
@@ -12,6 +12,7 @@ import { environment } from '../../../../../environments/environment';
 })
 export class ContactForm implements OnDestroy {
   private sendMailSubscription: Subscription | null = null;
+  protected successSection = viewChild.required<ElementRef<HTMLElement>>('success');
 
   constructor(private readonly httpClient: HttpClient) {}
 
@@ -22,6 +23,15 @@ export class ContactForm implements OnDestroy {
   protected submit(form: HTMLFormElement) {
     const data = new FormData(form);
 
-    this.sendMailSubscription = this.httpClient.post(`${environment.apiUrl}/send-mail`, data).subscribe();
+    this.sendMailSubscription = this.httpClient.post(`${environment.apiUrl}/send-mail`, data).subscribe({
+      next: res => {
+        this.toggleSuccessSection();
+        setTimeout(() => this.toggleSuccessSection(), 5000);
+      }
+    });
+  }
+
+  private toggleSuccessSection(): void {
+    this.successSection().nativeElement.classList.toggle('flex');
   }
 }
