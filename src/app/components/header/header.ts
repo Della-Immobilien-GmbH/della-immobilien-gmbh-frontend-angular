6e4ded274936a2b0d5926bfd90dd1ba2e4daefd7
Tom <tom.weelborg@gmail.com>
2025-12-26 03:26:06 +0100

    feat: add responsive nav bar

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+  protected faBars = faBars;
+  protected nav = viewChild.required<ElementRef<HTMLElement>>('nav');
 
+  protected toggleNav(): void {
+    this.nav().nativeElement.classList.toggle('open');
+  }
 }
